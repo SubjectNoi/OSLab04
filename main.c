bcb079c7f83eee6890b84ebe34b43932fd50264b
Subject_No_i <ilovehanhan1120@hotmail.com>
2017-12-08 01:14:56 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include "counter.h"
 #include "list.h"
 #include "hash.h"
+#include <sys/time.h>
 
 int thread_count;
 
@@ -15,19 +17,44 @@ void* doanything(void*);
 void* donothing(void*);
 
 int main(int argc, char* argv[]) {
+    struct timeval st, ed;
     srand(time(NULL));
     counter_init(&cnt, 0);
     list_init(&lst);
-    hash_init(&hsh, 117);
+    hash_init(&hsh, 1500);
 
     pthread_t       *thread_handler;
     thread_handler = (pthread_t*)malloc(sizeof(pthread_t) * 1024);
     thread_count = strtol(argv[1], NULL, 10);
     long            thread;
-    clock_t st, ed;
-    int i, sum = 0;
 
-    st = clock();
+    int i;
+
+    gettimeofday(&st, NULL);
+    for (thread = 0; thread < thread_count; thread++) {
+        pthread_create(&thread_handler[thread], NULL, dosomething, (void *) thread);
+    }
+
+    for (thread = 0; thread < thread_count; thread++) {
+        pthread_join(thread_handler[thread], NULL);
+    }
+    gettimeofday(&ed, NULL);
+    printf("counter takes %d us.\n", (ed.tv_sec - st.tv_sec) * 1000000 + (ed.tv_usec - st.tv_usec));
+
+
+    gettimeofday(&st, NULL);
+    for (thread = 0; thread < thread_count; thread++) {
+        pthread_create(&thread_handler[thread], NULL, doanything, (void *) thread);
+    }
+
+    for (thread = 0; thread < thread_count; thread++) {
+        pthread_join(thread_handler[thread], NULL);
+    }
+    gettimeofday(&ed, NULL);
+    printf("counter takes %d us.\n", (ed.tv_sec - st.tv_sec) * 1000000 + (ed.tv_usec - st.tv_usec));
+
+
+    gettimeofday(&st, NULL);
     for (thread = 0; thread < thread_count; thread++) {
         pthread_create(&thread_handler[thread], NULL, donothing, (void *) thread);
     }
@@ -35,12 +62,8 @@ int main(int argc, char* argv[]) {
     for (thread = 0; thread < thread_count; thread++) {
         pthread_join(thread_handler[thread], NULL);
     }
-
-    ed = clock();
-    sum += (ed - st);
-
-    printf("Takes %d cycles.\n", sum);
-
+    gettimeofday(&ed, NULL);
+    printf("counter takes %d us.\n", (ed.tv_sec - st.tv_sec) * 1000000 + (ed.tv_usec - st.tv_usec));
 
     return 0;
 }
@@ -48,8 +71,11 @@ int main(int argc, char* argv[]) {
 void* dosomething(void* rank) {
     long my_rank = (long) rank;
     int i;
-    for (i = 0; i < 1000000; i++) {
+    for (i = 0; i < 250000; i++) {
         counter_increment(&cnt);
+    }
+    for (i = 0; i < 250000; i++) {
+        counter_decrement(&cnt);
     }
 }
 
@@ -59,22 +85,19 @@ void* doanything(void* rank) {
     for (i = 0; i < 10000; i++) {
         list_insert(&lst, i);
     }
-    for (i = 0; i < 5000; i++) {
-        list_delete(&lst, random() % 10000);
+    for (i = 0; i < 10000; i++) {
+        list_delete(&lst, i);
     }
 }
 
 void* donothing(void* rank) {
     long my_rank = (long) rank;
     int i;
-    for (i = 0; i < 2000; i++) {
+    for (i = 0; i < 100000; i++) {
         hash_insert(&hsh, i);
     }
-    hash_print(&hsh);
-    printf("------------------------------------------------------------------\n");
-    for (i = 0; i < 1000; i++) {
-        hash_delete(&hsh, random() % 2000);
+    for (i = 0; i < 100000; i++) {
+        hash_delete(&hsh, i);
     }
-    hash_print(&hsh);
 }
 
